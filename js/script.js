6b15f10e8f38522ea853de65ab89c3ad35e8d7cf
Alex Zelenak <alexzelenak@gmail.com>
2011-12-12 02:41:44 -0600

    form validation part dos

--- js/script.js
+++ js/script.js
@@ -13,35 +13,45 @@ $(document).ready(function(){
 		}
 	});
 	
+	$('form input').bind("focus change", function(){
+		$(this).closest('.rsvp-block').removeClass('rsvp-invalid');
+	});
+	
 	$('form').submit(function(){
 		var invalid = [];
 		// there is a name
 		if ( !$.trim( $('.rsvp-name input').val() ).length ) {
 			invalid.push("You must enter a name.");
+			$('.rsvp-name input').closest('.rsvp-block').addClass('rsvp-invalid');
 		}
 		
 		// there is an email & and it has an @
 		if ( !$.trim( $('.rsvp-email input').val() ).length ) {
 			invalid.push("You must enter an email.");
+			$('.rsvp-email input').closest('.rsvp-block').addClass('rsvp-invalid');
 		}
 		else {
 			if ( !~$('.rsvp-email input').val().indexOf('@') ) {
 				invalid.push("Bad Email, yo");
+				$('.rsvp-email input').closest('.rsvp-block').addClass('rsvp-invalid');
 			}
 		}
 		
 		// there is an attendance
 		if ( !$('.rsvp-attend-yes input').is(':checked') && !$('.rsvp-attend-no input').is(':checked')  ) {
 			invalid.push("Let us know frealz");
+			$('.rsvp-attend-yes input').closest('.rsvp-block').addClass('rsvp-invalid');
 		}
 		else {
 			if ( ! $('.rsvp-party .ss-choices input:checked').length ) {
 				invalid.push("How many?");
+				$('.rsvp-party input').closest('.rsvp-block').addClass('rsvp-invalid');
 			}
 			else {
 				if ( $('.rsvp-party-other input').first().is(':checked') ) {
 					if ( !$.trim($('.ss-q-other').val()).length ) {
 						invalid.push("What other?");
+						$('.rsvp-party-other input').closest('.rsvp-block').addClass('rsvp-invalid');
 					}
 				}
 			}
@@ -49,7 +59,6 @@ $(document).ready(function(){
 			// if attendance - there is a number selected
 	
 		if ( invalid.length ) {
-			$('.rsvp-block').addClass('rsvp-invalid');
 			return false;
 		}
 		
