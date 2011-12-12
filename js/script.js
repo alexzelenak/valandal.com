9baf6e86460502bdad0129fe61717b8cdeb3f7e8
Alex Zelenak <alexzelenak@gmail.com>
2011-12-11 23:48:43 -0600

    conditional party field for rsvp

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-/* Author: 
+/* Author: A-Zel */
 
-*/
-
-
+$(document).ready(function(){
+	
+	$('.rsvp-attend-yes input').focus(function(){
+		$('.rsvp-party').addClass('form-section-focus');
+	});
+	$('.rsvp-attend-no input').focus(function(){
+		$('.rsvp-party').removeClass('form-section-focus');
+	});
+	
+});
 
 
 
