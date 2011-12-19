1bb515876797b80bfa6d3f34f0bce01167351e4e
Alex Zelenak <alexzelenak@gmail.com>
2011-12-19 05:07:47 -0600

    new kiss

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ $(document).ready(function(){
 
 	$(function() {
 		//this code will run when the page is done loading
-		var seconds = 0; //this is how long the page will wait
+		var seconds = 1; //this is how long the page will wait
 			window.setTimeout(function() {
 			//this code will run after a timeout
 			$("#muah img").addClass("tween");
